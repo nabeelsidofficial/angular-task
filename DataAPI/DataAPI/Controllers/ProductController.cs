b26e16645f12a3f35bc147e01f511aa7327bd418
M.Nabeel Siddiqui <nabeel.6303@outlook.com>
2024-02-10 12:35:29 -0800

    dto changes and added singleton products class.

--- DataAPI/DataAPI/Controllers/ProductController.cs
+++ DataAPI/DataAPI/Controllers/ProductController.cs
@@ -8,36 +8,28 @@ namespace DataAPI.Controllers
     public class ProductController : ControllerBase
     {
         private readonly ILogger<ProductController> _logger;
-
-        private List<Product> products = new List<Product>() {
-                new Product(){Id= 0,Price=10,Quantity=50, ProductName="Product A" },
-                new Product(){Id= 1,Price=11,Quantity=72, ProductName="Product B" },
-                new Product(){Id= 2,Price=12,Quantity=89, ProductName="Product C" },
-                new Product(){Id = 3, Price=13,Quantity=34, ProductName="Product D" },
-                new Product(){Id = 4, Price=14,Quantity=53, ProductName="Product E" },
-                new Product(){Id = 5, Price=15,Quantity=63, ProductName="Product F" }
-            };
-        public ProductController(ILogger<ProductController> logger)
+        private readonly SingletonProduct _singleton_product;
+        public ProductController(ILogger<ProductController> logger, SingletonProduct singleton_product)
         {
             _logger = logger;
+            _singleton_product = singleton_product;
         }
-
         [HttpGet]
         [Route("GetProducts")]
         public IEnumerable<Product> GetProducts()
         {
-            return products;
+            return _singleton_product.products;
         }
         [HttpPost]
         [Route("UpdateProduct")]
         public IEnumerable<Product> UpdateProduct([FromBody] RequestProductDTO requestProduct)
         {
-            var product = products.FirstOrDefault(x => x.Id == requestProduct.id);
+            var product = _singleton_product.products.FirstOrDefault(x => x.Id == requestProduct.id);
             if (product != null)
             {
                 product.ProductName = requestProduct.productName;
             }
-            return products;
+            return _singleton_product.products;
         }
     }
 }
--- DataAPI/DataAPI/Infrastructure/Models/Products/Product.cs
+++ DataAPI/DataAPI/Infrastructure/Models/Products/Product.cs
@@ -1,5 +1,16 @@
 ﻿namespace DataAPI.Infrastructure.Models.Products
 {
+    public class SingletonProduct
+    {
+        public List<Product> products = new List<Product>() {
+                new Product(){Id= 0,Price=10,Quantity=50, ProductName="Product A" },
+                new Product(){Id= 1,Price=11,Quantity=72, ProductName="Product B" },
+                new Product(){Id= 2,Price=12,Quantity=89, ProductName="Product C" },
+                new Product(){Id = 3, Price=13,Quantity=34, ProductName="Product D" },
+                new Product(){Id = 4, Price=14,Quantity=53, ProductName="Product E" },
+                new Product(){Id = 5, Price=15,Quantity=63, ProductName="Product F" }
+            };
+    }
     public class Product
     {
         public int Id { get; set; }
